7c68d0a1ba42f7dd267812f738e989e367cfc109
Farid Aliyev <farid.aliyevv258@gmail.com>
2024-07-01 10:44:52 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+(function(){
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i=0; i<names.length; i++) {
+
+  var firstLetter = String(names[i]).charAt(0);
+
+
+  if (firstLetter.toLowerCase() == 'j') {
+    byeSpeaker.speak(names[i]);
+  } else {
+    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[i]);
+  }
+}
+})();
